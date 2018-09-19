3a9dc3068bd41a5a0d071e3dfd9868acb06e4bf7
shanejonas <jonas.shane@gmail.com>
2018-09-19 11:00:13 -0700

    fix: testrpc -- sputnikvm-dev linux support

--- downloaders/sputnikvm-dev.js
+++ downloaders/sputnikvm-dev.js
@@ -1,10 +1,17 @@
 const request = require('request');
 const tar = require('tar');
 const path = require('path');
+const os = require('os');
 const extract = tar.x({
   cwd: path.resolve(__dirname, '../')
 });
 
 console.log('downloading and unpacking sputnikvm-dev please wait...');
+const platform = os.platform()
 
-request('http://builds.etcdevteam.com/sputnikvm-dev/v0.2.x/svmdev-osx-v0.2.0-36-46a63e0.tar.gz').pipe(extract);
+switch (platform) {
+    case 'darwin': 
+      return request('http://builds.etcdevteam.com/sputnikvm-dev/v0.2.x/svmdev-osx-v0.2.0-36-46a63e0.tar.gz').pipe(extract);
+    case 'linux': 
+      return request('http://builds.etcdevteam.com/sputnikvm-dev/v0.2.x/svmdev-linux-v0.2.0-36-46a63e0.tar.gz').pipe(extract);
+}
